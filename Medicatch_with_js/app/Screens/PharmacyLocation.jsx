5fc5d6b1a609896d037f3b974917430da7e8073d
ahmad122519 <ahmadali122519@gmail.com>
2025-01-26 16:31:35 +0500

    api key added

--- Medicatch_with_js/app/Screens/PharmacyLocation.jsx
+++ Medicatch_with_js/app/Screens/PharmacyLocation.jsx
@@ -73,7 +73,7 @@ const PharmacyLocation = ({ onConfirm }) => {
             handleLocationSelect(lat, lng);
           }}
           query={{
-            key: "AIzaSyBENAB06JOT3-qUoLm-fNqLZDMCsca6BKk", // Replace with your API key
+            key: "AIzaSyCUVQfI-8gQf0KrD9-tPVxNcTGwqlN-pNU", // Replace with your API key
             language: "en",
           }}
           fetchDetails={true}
